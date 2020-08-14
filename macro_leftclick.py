4bf54b85dc05cbfab96b1502385bfecd983c8853
TidosDK <git@msnn.dev>
2020-08-14 20:38:54 +0200

    Add files via upload

--- macro_leftclick.py
+++ macro_leftclick.py
@@ -1,36 +1,39 @@
-###################
-##### IMPORTS #####
-###################
+# Copyright by Mathias Nicolajsen
+# Code by Mathias Nicolajsen
+
 from pynput.mouse import Button, Controller
 import keyboard
 import time
 
-
-#####################
-##### VARIABLES #####
-#####################
+# Variables
 mouse = Controller()
 start_key = '+'
 stop_key = '-'
 status = False
-macro_time = 0.01
 
+# Asks if user wants to use left or right click as macro
+print("\nWould you like to use right or left click as macro? (right/left)\n")
+msg = input(">>").lower()
 
-###################
-##### WELCOME #####
-###################
-print(" ")
-print(" Python macro")
-print(f" Macro wait time: {macro_time}")
-print(" ")
-print(" Start: +")
-print(" Stop: -")
-print(" ")
+# Checks what the user said
+if msg == "right" or msg == "r":
+    buttonUse = Button.right
+elif msg == "left" or msg == "l":
+    buttonUser = Button.left
+else:
+    raise Exception("You can only use right or left")
 
+# Asks user how many seconds there should be between each click
+print("\n\nHow much time should there be between each click? (0.01)\n")
+macro_time = float(input(">>"))
 
-###########################
-##### MACRO MECHANICS #####
-###########################
+# Tells user about how to use the macro
+print("\n\n\nMacro wait time: {0}\n".format(macro_time))
+print("Start: +")
+print("Stop: -")
+print("\nExit: del key")
+
+# Run the macro program
 while True:
     if status == True:
         mouse.click(Button.left)
@@ -40,4 +43,8 @@ while True:
     elif keyboard.is_pressed(start_key) and status == False:
         print("(+) Macro has started")
         status = True
-    time.sleep(macro_time)
+    elif keyboard.is_pressed("del"):
+        print("(X) Stopping program")
+        break
+
+    time.sleep(macro_time)  # Delay
